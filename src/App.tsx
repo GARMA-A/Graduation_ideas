69a583bc7db82fe971e151ca0f4d39559bfc411c
Girgis <girgisemad74@gmail.com>
2025-07-11 11:35:06 +0300

    make the smallNotes container scrollable

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 // import NavBar from "./components/NavBar"
-import { Box, Paper, Stack } from "@mui/material";
+import { Box, Paper, Stack, useMediaQuery, useTheme } from "@mui/material";
 import NavBar from "./components/NavBar";
 import SearchBar from "./components/SearchBar";
 import SmallNote from "./components/SmallNote";
 import SmallNoteContainer from "./components/SmallNotesContainer";
 import { ColorModeProvider } from "./contexts/ThemeContext";
+import BottomNavBar from "./components/BottomNavBar";
 
 
 function App() {
+
+
+  const theme = useTheme();
+  const isSm = useMediaQuery(theme.breakpoints.down('sm'));
+
+
+
   return (<ColorModeProvider>
-    <NavBar />
+    {!isSm && <NavBar />}
+    {isSm && <BottomNavBar />}
 
     <Box
       component="main"
@@ -35,6 +44,26 @@ function App() {
             <SmallNote title="the secont title "
               description="This is a description for the second note. It should be short and concise."
             />
+            <SmallNote title="the third title this title will ne used to check the how many words i can put in the title without over lap"
+              description="This is a description for the third note. It should be short and concise."
+            />
+
+            <SmallNote title="the third title this title will ne used to check the how many words i can put in the title without over lap"
+              description="This is a description for the third note. It should be short and concise."
+            />
+
+            <SmallNote title="the third title this title will ne used to check the how many words i can put in the title without over lap"
+              description="This is a description for the third note. It should be short and concise."
+            />
+
+            <SmallNote title="the third title this title will ne used to check the how many words i can put in the title without over lap"
+              description="This is a description for the third note. It should be short and concise."
+            />
+
+            <SmallNote title="the third title this title will ne used to check the how many words i can put in the title without over lap"
+              description="This is a description for the third note. It should be short and concise."
+            />
+
             <SmallNote title="the third title this title will ne used to check the how many words i can put in the title without over lap"
               description="This is a description for the third note. It should be short and concise."
             />
--- src/components/SmallNotesContainer.tsx
+++ src/components/SmallNotesContainer.tsx
@@ -1,4 +1,4 @@
-import { Box, Dialog } from "@mui/material";
+import { Box } from "@mui/material";
 
 type SmallNoteContainerProps = {
   children: React.ReactNode;
@@ -9,13 +9,24 @@ export default function SmallNoteContainer({ children }: SmallNoteContainerProps
   return (
     <Box
       display="flex"
-      justifyContent="center"
       flexDirection="column"
-      width="50vw"
+      alignItems="stretch"
       gap={1}
+      sx={{
+        width: { xs: '100%', sm: '100%', md: '80vw', lg: '65vw' },
+        maxHeight: '500px',
+        overflowY: 'auto',
+        '&::-webkit-scrollbar': {
+          width: '8px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: '#888',
+          borderRadius: '4px',
+        },
+      }}
     >
       {children}
-    </Box>
+    </Box >
 
   );
 
