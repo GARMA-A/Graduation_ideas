065d1c96188182fd98730c62c951155619f0a63a
Girgis <girgisemad74@gmail.com>
2025-07-13 10:06:42 +0300

    move some files and folders restructure the project create the store using redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { Provider } from "react-redux";
 import { ColorModeProvider } from "./contexts/ThemeContext";
 import HomePage from "./pages/HomePage";
+import store from "./store.tsx";
 // import HomePage from "./pages/HomePage";
-
 
 function App() {
   return (
-    <ColorModeProvider>
-      <HomePage />
-    </ColorModeProvider>
+    <Provider store={store}>
+      <ColorModeProvider>
+        <HomePage />
+      </ColorModeProvider>
+    </Provider>
   );
 
 }
---
+++ src/store.tsx
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { noteSlice } from './features/Notes/noteSlice';
+
+
+
+
+const store = configureStore({
+  reducer: {
+    notes: noteSlice.reducer,
+  }
+})
+
+export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
